0074375498404f7adf5ebc51ed93e2234bce9850
Giorgi Arevadze <gioarevadze97@gmail.com>
2024-03-10 22:05:50 +0400

    added tracking

--- app/models.py
+++ app/models.py
@@ -29,3 +29,16 @@ class WorkoutPlan(db.Model):
 
     def __repr__(self):
         return f"WorkoutPlan(id={self.id}, user_id={self.user_id})"
+
+
+
+class TrackingAndGoals(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    fitness_goals = db.Column(db.Text)
+    achievements = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"TrackingAndGoals(id={self.id}, user_id={self.user_id}, date={self.date}, weight={self.weight})"
